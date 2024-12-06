689d9e4e420a85bd24b427bfc91005d6659b2be4
ionelaamatei2004 <ionelaamatei2004@gmail.com>
2024-12-06 17:02:22 +0200

    Design pentru paginile de autentificare si inregistrare

---
+++ AplicatieLicenta/Pages/Users/ParolaUitata.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AplicatieLicenta.Pages.Users
+{
+    public class ParolaUitataModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ AplicatieLicenta/wwwroot/js/login.js
@@ -0,0 +1,13 @@
+﻿const container = document.getElementById('container');
+const signInReaderButton = document.getElementById('signInReader');
+const signInAdminButton = document.getElementById('signInAdmin');
+
+// Activează formularul de cititor
+signInReaderButton.addEventListener('click', () => {
+    container.classList.remove('right-panel-active');
+});
+
+// Activează formularul de administrator
+signInAdminButton.addEventListener('click', () => {
+    container.classList.add('right-panel-active');
+});
